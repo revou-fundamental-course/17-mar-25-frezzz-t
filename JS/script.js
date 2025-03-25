e6fb12967d780c7ab8c15f425655675c2552dd2a
frezzz-t <dekpiteuku@gmail.com>
2025-03-25 22:07:27 +0700

    up prog 03

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
 alert("Kamu Hitam")
+button 
+document.addEventListener("DOMContentLoaded", function() {
+    var button1 = document.getElementById("button1");
+    var button2 = document.getElementById("button2");
+
+    button1.addEventListener("click", function() {
+        window.location.href = "Assets/P.panjang.html"; // Arahkan ke page1.html
+    });
+    button2.addEventListener("click", function() {
+        window.location.href = "Assets\P.panjang.html"; // Arahkan ke page2.html
+    });
+});
